95211688dc433b6bed96b389cab80d21c206a4db
Day <dayannefernandesc@gmail.com>
2017-03-27 22:00:01 -0300

    add event animator when click on chest

---
+++ Assets/playChest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playChest : MonoBehaviour {
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+		{
+			Animator anim = GetComponent<Animator>();
+			if (null != anim)
+			{
+				Debug.Log("Playing anim");
+				anim.SetTrigger("openchest");
+			}
+		}
+	}
+}
+	
