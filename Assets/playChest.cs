7480acd9d523fd5df9896d59c5904ab9adca1656
Day <dayannefernandesc@gmail.com>
2017-04-10 21:10:50 -0300

    add new images to show after open the chest

--- Assets/playChest.cs
+++ Assets/playChest.cs
@@ -1,17 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class playChest : MonoBehaviour {
+public class playChest : MonoBehaviour {	
+	public GameObject card_image;
+	public Image image;
+
+	void Start(){
+		card_image = GameObject.Find("canvas_card");
+		card_image.SetActive (false);
+	}
+
 	void Update () {
-		if(Input.GetMouseButtonDown(0))
-		{
-			Animator anim = GetComponent<Animator>();
-			if (null != anim)
-			{
-				Debug.Log("Playing anim");
-				anim.SetTrigger("openchest");
+		Animator anim = GetComponent<Animator> ();
+		if (Input.GetMouseButtonDown (0)) {
+			if (null != anim) {
+				if (anim.GetBool ("chest_status")) {
+					anim.SetTrigger ("openchest");
+					card_image.SetActive (true);
+					anim.SetTrigger ("showimage");
+					anim.SetBool ("chest_status", false);
+				} else {
+					anim.SetTrigger ("closechest");
+					anim.SetBool ("chest_status", true);
+					card_image.SetActive (false);
+				}
 			}
 		}
 	}
 }
+
 	
---
+++ Assets/showCard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showCard : MonoBehaviour {
+	public GameObject card_object;
+
+	void Start(){
+	}
+
+	void Update () {
+	}
+}
