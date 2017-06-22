7b891f85599176e7aacd79ebb9a7fc40918b3881
Day <dayannefernandesc@gmail.com>
2017-06-22 20:54:02 -0300

    temp animator

--- Assets/playChest.cs
+++ Assets/playChest.cs
@@ -5,27 +5,28 @@ using UnityEngine.UI;
 public class playChest : MonoBehaviour {	
 	public GameObject card_image;
 	public Image image;
+	public Animator anim;
 
 	void Start(){
-		card_image = GameObject.Find("canvas_card");
-		card_image.SetActive (false);
+		anim = GetComponent<Animator> ();
+		anim.Play ("hide_card", -1, 0f);
 	}
 
 	void Update () {
-		Animator anim = GetComponent<Animator> ();
+		
 		if (Input.GetMouseButtonDown (0)) {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (Physics.Raycast(ray, 100)) {
 				if (null != anim) {
 					if (anim.GetBool ("chest_status")) {
-						anim.SetTrigger ("openchest");
 						card_image.SetActive (true);
-						anim.SetTrigger ("showimage");
+						anim.Play ("box_open", -1, 0f);
+						anim.Play ("show_image", -1, 0f);
 						anim.SetBool ("chest_status", false);
 					} else {
-						anim.SetTrigger ("closechest");
+						anim.Play ("box_close", -1, 0f);
+						anim.Play ("card_to_bag", -1, 0f);
 						anim.SetBool ("chest_status", true);
-						card_image.SetActive (false);
 					}
 				}
 			}
